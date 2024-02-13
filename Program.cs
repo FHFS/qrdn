114a1a016361912c616256f7277f36142b2cca7f
fhfs <frank.schrijver@gmail.com>
2024-02-13 11:13:26 +0100

    Compiling ok, nuill ref at runtime in controller

--- Program.cs
+++ Program.cs
@@ -1,6 +1,80 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Drawing;
 
-app.MapGet("/", () => "Hello World!");
+namespace QRdn
+{
+    public class QRCodeController : Controller
+    {
+        public IActionResult Index(string data)
+        {
+            // Generate QRCode image
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
 
-app.Run();
+            // Convert Bitmap to byte array
+            byte[] byteArray;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                qrCodeImage.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                byteArray = stream.ToArray();
+            }
+
+            // Return the image as a FileResult
+            return File(byteArray, "image/png");
+        }
+    }
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
+                    name: "qrcode",
+                    pattern: "a",
+                    defaults: new { controller = "QRCode", action = "Index" });
+            });
+        }
+    }
+
+    public class Program
+    {
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+                webBuilder.UseUrls("http://localhost:89");
+            });
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("QR dotNet started");
+            CreateHostBuilder(args).Build().Run();
+            Console.ReadLine();
+        }
+
+    }
+}
